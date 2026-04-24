e6fce44f7cb5946604a70efeb28182f2794065f7
蜡笔小强 <89714521+Asyncload@users.noreply.github.com>
2026-04-24 22:24:19 +0800

    Update and rename build.gradle to build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.lbxq.screen"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.lbxq.screen"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        buildConfig = false
+    }
+
+    lint {
+        abortOnError = false
+        ignoreWarnings = true
+        checkReleaseBuilds = false
+    }
+}
+
+dependencies {
+    implementation("de.robv.android.xposed:api:82")
+    implementation("de.robv.android.xposed:api-static:82")
+}
